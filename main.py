3a39436cfc99200bf07730d14cde78df7c10d94f
pedrocarlo <pedro.muniz.carlo@gmail.com>
2023-09-01 02:46:24 +0000

    managed to correctly manifest automatically and download notas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 from typing import Annotated, Optional
 from fastapi import FastAPI, BackgroundTasks, Depends, HTTPException
@@ -9,7 +10,13 @@ from db.sql import get_general, get_session
 # from db.schemas import NfeQueryParams
 
 # from db.sql import insert_xml_from_folder
-from hotel_api.tasks import novas_notas, test, test_nota
+from hotel_api.tasks import (
+    download_completa,
+    manifestar,
+    novas_notas,
+    test,
+    test_get_chave,
+)
 import logging
 
 
@@ -27,13 +34,13 @@ app = FastAPI()
 cwd = os.getcwd()
 
 
-@app.get("/", status_code=202, tags=["info"])
-async def root():
-    logger.info("Calling /")
-    ult_nsu, xmls, notas = test.delay().get()
-    # logger.info("%s", notas)
-    # logger.info("%s", ult_nsu)
-    return {"message": "Adding files to database"}
+# @app.get("/", status_code=202, tags=["info"])
+# async def root():
+#     logger.info("Calling /")
+#     ult_nsu, xmls, notas = test.delay().get()
+#     # logger.info("%s", notas)
+#     # logger.info("%s", ult_nsu)
+#     return {"message": "Adding files to database"}
 
 
 @app.get("/novas_notas", status_code=202, tags=["info"])
@@ -43,6 +50,20 @@ async def novas():
     logger.info("XMLS: \n%s", xmls)
     # logger.info("%s", ult_nsu)
     return {"message": "getting novas notas to database"}
+
+
+@app.get("/manifestar", status_code=202, tags=["info"])
+async def manifestar_notas(year: int = None, month: int = None):
+    today = datetime.now()
+    curr_year = today.year
+    curr_month = today.month
+    if year is None:
+        year = curr_year
+    if month is None:
+        month = curr_month
+    notas = await manifestar(year, month)
+    logger.info("Notas: %s", notas)
+    return {"202": "Accepted"}
 
 
 @app.get("/filtrar_notas", status_code=202, tags=["info"])
@@ -61,6 +82,17 @@ async def filtrar_notas(params: NfeQueryParams = Depends()):
     dictret = dict(params.__dict__)
     dictret.pop("_sa_instance_state", None)
     return query
+
+
+@app.get("/get_completa", status_code=202, tags=["info"])
+async def completa():
+    return download_completa()
+
+
+@app.get("/test_chave", status_code=202, tags=["info"])
+async def completa(chave: str):
+    # chave = "35230871998819000138550010000022771002245280"
+    return test_get_chave(chave)
 
 
 if __name__ == "__main__":
--- tasks.py
+++ tasks.py
@@ -3,9 +3,10 @@ import logging
 from celery import Celery
 import celery
 from sefaz.distNfe import distNfe
+from sefaz.manifestarNfe import manifestNfe
 from sefaz.xml_parser import get_tags
 from db.model import Nfe
-from db.sql import Session, get_session
+from db.sql import Session, get_by_date, get_manifestando, get_session
 from lxml import etree
 import os
 from celery.utils.log import get_task_logger
@@ -26,6 +27,8 @@ app.conf.update(
     enable_utc=False,
 )
 
+CODIGO_REJEICAO = 201
+
 
 @celery.signals.after_setup_logger.connect
 def on_after_setup_logger(**kwargs):
@@ -39,13 +42,19 @@ def on_after_setup_logger(**kwargs):
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    h1_3min = 3780
+    h1_3min = 3780  # 1 hour 3 minutes in seconds
     sender.add_periodic_task(h1_3min, novas_notas.s(), name="Ler novas notas")
+    sender.add_periodic_task(
+        h1_3min, download_completa.s(), name="Baixar notas completas"
+    )
+    sender.add_periodic_task(
+        h1_3min, manifest_start_month.s(), name="Manifestar mes passado"
+    )
 
 
 @app.task
 def novas_notas():
-    CODIGO_REJEICAO = 201
+    CNPJ = "51548782000139"
     with open(os.path.join(cwd, "celery", "ultNSU.txt"), "r") as f:
         nsu = int(f.readline().strip())
     ult_nsu = nsu
@@ -71,28 +80,13 @@ def novas_notas():
                         logger.warning("NOTA REPR: %s", repr(nota))
                         session.merge(nota)
                         session.commit()
+                        ciencia_emissao = 2
+                        manifestNfe(nota.chave, CNPJ, ciencia_emissao)
                     finally:
                         session.close()
                     # with Session() as session:
                     #     session.add(nota)
     return ult_nsu, xmls, notas
-
-
-def write_xml(xml: str, nota: Nfe):
-    if nota is not None:
-        folder = nota.get_folder()
-        path = os.path.join(cwd, "xml", folder, f"{nota.chave}.xml")
-    else:
-        folder = "outros"
-        path = os.path.join(cwd, "xml", folder, f"{datetime.now()}.xml")
-    resposta = etree.fromstring(xml)
-    resposta.getroottree().write(path, pretty_print=True)
-    return path
-
-
-def write_ult_nsu(nsu: int):
-    with open(os.path.join(cwd, "celery", "ultNSU.txt"), "w") as f:
-        f.write(str(nsu))
 
 
 @app.task
@@ -131,10 +125,93 @@ def test():
 
 
 @app.task
-def test_nota():
-    chave = "35230624614269000126550010003386141336538648"
+def download_completa():
+    session = get_session()
+    session.expire_on_commit = False
+    notas = get_manifestando(session)
+    completos = []
+    for nota in notas[:20]:
+        ult_nsu, max_nsu, codigo, xmls = distNfe(
+            nota.chave, 0, is_nsu=False, is_nsu_especifico=False
+        )
+        if codigo < CODIGO_REJEICAO:
+            for xml in xmls:
+                completos.append(xml)
+                new_nota = get_tags(xml_str=xml)
+                path = write_xml(xml, new_nota)
+                try:
+                    logger.warning("NOTA REPR: %s", repr(new_nota))
+                    session.merge(new_nota)
+                    session.commit()
+                finally:
+                    session.close()
+    return completos
+
+
+@app.task
+def test_get_chave(chave: str):
+    session = get_session()
+    session.expire_on_commit = False
+    completos = []
     ult_nsu, max_nsu, codigo, xmls = distNfe(
-        chave, 4090, is_nsu=True, is_nsu_especifico=True
+        chave, 0, is_nsu=False, is_nsu_especifico=False
     )
-    logger.info("ULT_NSU: %s  MAX_NSU: %s", ult_nsu, max_nsu)
-    return ult_nsu, xmls
+    if codigo < CODIGO_REJEICAO:
+        for xml in xmls:
+            completos.append(xml)
+            new_nota = get_tags(xml_str=xml)
+            path = write_xml(xml, new_nota)
+            try:
+                logger.warning("NOTA REPR: %s", repr(new_nota))
+                session.merge(new_nota)
+                session.commit()
+            finally:
+                session.close()
+    return completos
+
+
+@app.task
+async def manifest_start_month():
+    today = datetime.now()
+    day = today.day
+    month = today.month
+    year = today.year
+    if day == 1:
+        return await manifestar(year, month)
+    return []
+
+
+async def manifestar(year: int, month: int):
+    session = get_session()
+    notas = get_by_date(session, year, month)
+    CNPJ = "51548782000139"
+
+    print(notas)
+    session = get_session()
+    for nota in notas:
+        chave = nota.chave
+        await manifestNfe(chave, CNPJ)
+        try:
+            nota.manifestando = True
+            session.merge(nota)
+            session.commit()
+        finally:
+            session.close()
+    return notas
+
+
+def write_xml(xml: str, nota: Nfe = None):
+    if nota is not None:
+        folder = nota.get_folder()
+        path = os.path.join(cwd, "xml", folder, f"{nota.chave}.xml")
+    else:
+        folder = "outros"
+        path = os.path.join(cwd, "xml", folder, f"{datetime.now()}.xml")
+    resposta = etree.fromstring(xml)
+    resposta.getroottree().write(path, pretty_print=True)
+    return path
+
+
+def write_ult_nsu(nsu: int):
+    with open(os.path.join(cwd, "celery", "ultNSU.txt"), "w") as f:
+        f.write(str(nsu))
